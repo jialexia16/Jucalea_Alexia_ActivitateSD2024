ddacb1b0addd03a329f102b7464cb485d0347fb6
jialexia16 <anajucalea16@gmail.com>
2024-03-04 16:38:59 +0200

    modificare task (dupa seminar) + scriere in fisier

--- taskSuplimentar.c
+++ taskSuplimentar.c
@@ -50,30 +50,63 @@ void modificaPret(struct Masaj* m, float discount)
 
 
 
-void afiseazaMasaj(struct Masaj* m)
+void afiseazaMasaj(struct Masaj m)
 {
 
-	if (m->denumire != NULL)
-		printf("Nume: %s\n", m->denumire);
-	printf("Pret: %5.2f \n", m->pret);
-	printf("Nr uleiuri: %2d\n", m->nrUleiuri);
+	if (m.denumire != NULL)
+		printf("Nume: %s\n", m.denumire);
+	printf("Pret: %5.2f \n", m.pret);
+	printf("Nr uleiuri: %2d\n", m.nrUleiuri);
 	printf("Uleiuri: ");
-	for (int i = 0; i < m->nrUleiuri; i++)
+	for (int i = 0; i < m.nrUleiuri; i++)
 	{
 
-		printf("%4d ", m->codUleiuri[i]);
+		printf("%4d ", m.codUleiuri[i]);
 	}
 	printf("\n");
 }
 
-//Creati in functia main un vector alocat dinamic cu cel putin 5 obiecte de tipul structurii voastre.
-// functie care va creea un nou vector în care va copia dintr-un vector primit ca parametru obiectele care indeplinesc o anumita conditie
-// functie care muta intr-un nou vector obiectele care indeplinesc o alta conditie fata de cerinta precedenta
-//Realizati o functie care concateneaza doi vectori.
-//Realizati o functie care afiseaza un vector cu obiecte.
 
-struct Masaj* conditieNrMasaje(int nrMasaje, struct Masaj* masaje) {
-	struct Masaj* vectorNou = NULL;
+//afiseaza vector
+void afiseazaVector(struct Masaj* masaje, int nrMasaje)
+{
+	for (int i = 0; i < nrMasaje; i++)
+	{
+		printf("\n");
+		afiseazaMasaj(masaje[i]);
+	}
+}
+
+//copiaza Nelemente
+struct Masaj* copiazaNElemente(struct Masaj* masaje, int nrMasaje, int nrElementeCopiate) {
+	if (nrElementeCopiate <= nrMasaje && nrElementeCopiate > 0)
+	{
+		struct Masaj* copy = (struct Masaj*)malloc(sizeof(struct Masaj) * nrElementeCopiate);
+		for (int i = 0; i < nrElementeCopiate; i++)
+		{
+			copy[i] = initializareMasaj(masaje[i].denumire, masaje[i].pret, masaje[i].nrUleiuri, masaje[i].codUleiuri);
+		}
+		return copy;
+	}
+	else return NULL;
+}
+
+struct Masaj* conditieNrMasaje(int nrMasaje, struct Masaj* masaje, int* dimNoua, int pragUleiuriNecesare) {
+	*dimNoua = 0;
+	for (int i = 0; i < nrMasaje; i++)
+	{
+		if (masaje[i].nrUleiuri >= pragUleiuriNecesare) (*dimNoua)++;
+	}
+	struct Masaj* vectorNou = (struct Masaj*)malloc(sizeof(struct Masaj) * (*dimNoua));
+	int k = 0;
+	for (int i = 0; i < nrMasaje; i++)
+	{
+		if (masaje[i].nrUleiuri >= pragUleiuriNecesare) {
+			vectorNou[k] = initializareMasaj(masaje[i].denumire, masaje[i].pret, masaje[i].nrUleiuri, masaje[i].codUleiuri);
+			k++;
+		}
+	}
+	/*struct Masaj* vectorNou = NULL;
 	int nr = 0;
 
 	for (int i = 0; i < nrMasaje; i++) {
@@ -85,50 +118,133 @@ struct Masaj* conditieNrMasaje(int nrMasaje, struct Masaj* masaje) {
 			}
 			vectorNou[nr - 1] = masaje[i];
 		}
-	}
+	}*/
 
 	return vectorNou;
 }
 
+//deazlocare vector 
+void dezalocareVectorMasaje(struct Masaj** masaje, int* nrMasaje) {
+	//de ce am trimis nr masaje prin adresa la seminar?
+	for (int i = 0; i < *nrMasaje; i++)
+	{
+		free((*masaje)[i].denumire);
+		free((*masaje)[i].codUleiuri);
+	}
+	free(*masaje);
+	*nrMasaje = 0;
+	*masaje = NULL;
+}
+//get obiect by conditie
+struct Masaj getMasajByPrice(struct Masaj* masaje, int nrMasaje, float pret)
+{
+	for (int i = 0; i < nrMasaje; i++)
+	{
+		if (masaje[i].pret == pret) return initializareMasaj(masaje[i].denumire, masaje[i].pret, masaje[i].nrUleiuri, masaje[i].codUleiuri);
+	}
+	//returnam masaj default
+	return initializareMasaj("-", 0.0, 0, NULL);
+}
 
-//void main()
-//{
-//	int* coduri = malloc(sizeof(int) * 3);
-//	coduri[0] = 100;
-//	coduri[1] = 101;
-//	coduri[2] = 102;
-//	struct Masaj m1 = initializareMasaj("MASAJ1", 100.0, 3, coduri);
-//	afiseazaMasaj(&m1);
-//	printf("Pret mediu uleiuri:  %5.2f ", pretMediuUleiuri(m1));
-//	modificaPret(&m1, 10);
-//	free(coduri);
-//	int* coduri2 = malloc(sizeof(int) * 4);
-//	coduri2[0] = 200;
-//	coduri2[1] = 201;
-//	coduri2[2] = 202;
-//	coduri2[3] = 203;
-//	int* coduri3 = malloc(sizeof(int) * 3);
-//	coduri3[0] = 300;
-//	coduri3[1] = 301;
-//	coduri3[2] = 302;
-//
-//	//vector obiecte de tip Masaj
-//	struct Masaj* masaje = malloc(sizeof(struct Masaj) * 5);
-//	masaje[0] = m1;
-//	masaje[1] = initializareMasaj("MASAJ3", 300, 4, coduri2);
-//	masaje[2] = initializareMasaj("MASAJ3", 300, 4, coduri2);
-//	masaje[3] = initializareMasaj("MASAJ4", 400, 4, coduri2);
-//	masaje[4] = initializareMasaj("MASAJ5", 500, 3, coduri3);
-//
-//
-//	//functie creare vector care sa preia masajaele cu nrUleiuri mai mari decat 3
-//	printf("\n ------------------------CONDITIE--------------------\n");
-//	struct Masaj* masajeConditie = malloc(sizeof(conditieNrMasaje(5, masaje)));
-//	masajeConditie = conditieNrMasaje(5, masaje);
-//	for (int i = 0; i < 2; i++)
-//	{
-//		afiseazaMasaj(&masajeConditie[i]);
-//	}
-//	free(coduri2);
-//	free(coduri3);
-//}
+//Realizati o functie care concateneaza doi vectori.
+//Scrieti intr - un program C functia care sa citeasca obiectele din fisier si sa le salveze intr - un vector.
+
+
+
+//Scrieti o functie care va salva un obiect  intr - un fisier text.
+void scrieMasajInFisier(char* file, struct Masaj masaj)
+{
+	FILE* fp = fopen(file, "a");
+	if (fp == NULL)
+	{
+		printf("Eroare deschidere fisier %s", file);
+		return -1;
+	}
+	else {
+		fprintf(fp, "\n%-8s %5.2f %3d\n", masaj.denumire, masaj.pret, masaj.nrUleiuri);
+		for (int i = 0; i < masaj.nrUleiuri; i++)
+		{
+			fprintf(fp, "%2d ", masaj.codUleiuri[i]);
+		}
+	}
+	fclose(fp);
+}
+
+
+//Scrieti o functie care va salva un vector de obiecte intr - un fisier text.
+void scrieVectorMasajeInFisier(char* file, struct Masaj* masaje, int nrMasaje)
+{
+	FILE* fp = fopen(file, "w");
+	if (fp == NULL)
+	{
+		printf("Eroare deschidere fisier %s", file);
+		return -1;
+	}
+	else {
+		fprintf(fp, "%2d\n", 2 * nrMasaje);
+		for (int i = 0; i < nrMasaje; i++)
+		{
+			scrieMasajInFisier(file, masaje[i]);
+			scrieMasajInFisier(file, masaje[i]);
+			//am apelat de 2 ori ca eu am vector de 5 elemente si ati spus sa fie 10 obiecte in fisier 
+		}
+	}
+	fclose(fp);
+}
+
+
+void main()
+{
+	int* coduri = malloc(sizeof(int) * 3);
+	coduri[0] = 100;
+	coduri[1] = 101;
+	coduri[2] = 102;
+	struct Masaj m1 = initializareMasaj("MASAJ1", 100.0, 3, coduri);
+	afiseazaMasaj(m1);
+	printf("Pret mediu uleiuri:  %5.2f ", pretMediuUleiuri(m1));
+	modificaPret(&m1, 10);
+	free(coduri);
+	int* coduri2 = malloc(sizeof(int) * 4);
+	coduri2[0] = 200;
+	coduri2[1] = 201;
+	coduri2[2] = 202;
+	coduri2[3] = 203;
+	int* coduri3 = malloc(sizeof(int) * 3);
+	coduri3[0] = 300;
+	coduri3[1] = 301;
+	coduri3[2] = 302;
+
+	//vector obiecte de tip Masaj
+	int nrMasaje = 5;
+	struct Masaj* masaje = (struct Masaj*)malloc(sizeof(struct Masaj) * nrMasaje);
+	masaje[0] = m1;
+	masaje[1] = initializareMasaj("MASAJ2", 200, 4, coduri2);
+	masaje[2] = initializareMasaj("MASAJ3", 300, 4, coduri2);
+	masaje[3] = initializareMasaj("MASAJ4", 400, 4, coduri2);
+	masaje[4] = initializareMasaj("MASAJ5", 500, 3, coduri3);
+	printf("\n ------------------------VECTOR--------------------\n");
+	afiseazaVector(masaje, nrMasaje);
+	printf("\n ------------------------COPIEAZA N ELEMENTE--------------------\n");
+	int nrMasajeCopiate = 2;
+	struct Masaj* masajeCopiate = copiazaNElemente(masaje, 5, nrMasajeCopiate);
+	afiseazaVector(masajeCopiate, nrMasajeCopiate);
+
+	//functie creare vector care sa preia masajaele cu nrUleiuri mai mari decat 3
+	printf("\n ------------------------CONDITIE--------------------\n");
+	int nrMasajeScumpe = 0;
+	struct Masaj* masajeConditie = conditieNrMasaje(5, masaje, &nrMasajeScumpe, 4);
+	afiseazaVector(masajeConditie, nrMasajeScumpe);
+	printf("\n ------------------------MASAJ CAUTAT--------------------\n");
+	struct Masaj masajCautat = getMasajByPrice(masaje, nrMasaje, 90.0);
+	afiseazaMasaj(masajCautat);
+
+	//scrieMasajInFisier("data.txt", m1);
+	//Creati un fisier in care sa aveti minim 10 obiecte de tipul structurii create.Asezarea in fisier a elementelor o faceti la libera alegere.
+	scrieVectorMasajeInFisier("data.txt", masaje, nrMasaje);
+
+	dezalocareVectorMasaje(&masajeCopiate, &nrMasajeCopiate);
+	dezalocareVectorMasaje(&masajeConditie, &nrMasajeScumpe);
+	dezalocareVectorMasaje(&masaje, &nrMasaje);
+	free(coduri2);
+	free(coduri3);
+}
